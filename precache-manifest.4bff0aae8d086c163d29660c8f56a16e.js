259e5997188de075cac425d4ba88f4a2d6411c6a
Spendid-Infosystems-2023 <85026451+dnyanesh121@users.noreply.github.com>
2023-12-09 20:47:40 +0530

    Initial commit

---
+++ precache-manifest.4bff0aae8d086c163d29660c8f56a16e.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad48757576cfb73790b2065f59507d70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e90b834efed211ddcbd",
+    "url": "/static/css/main.0009c713.chunk.css"
+  },
+  {
+    "revision": "22b11d5aac1bd7f89df1",
+    "url": "/static/js/2.4312d964.chunk.js"
+  },
+  {
+    "revision": "176c1a1a28b19c611d9701905d8d60b7",
+    "url": "/static/js/2.4312d964.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e90b834efed211ddcbd",
+    "url": "/static/js/main.6e6bdfae.chunk.js"
+  },
+  {
+    "revision": "06bbfc6877769458ef8a",
+    "url": "/static/js/runtime-main.dda47e7a.js"
+  },
+  {
+    "revision": "b0de9caa77460a4e81ce56534783b70b",
+    "url": "/static/media/AllCountryInformation.b0de9caa.webp"
+  },
+  {
+    "revision": "9694a0bb1cf2abd6722f4ba450aa8d33",
+    "url": "/static/media/GitHubJobs.9694a0bb.webp"
+  },
+  {
+    "revision": "21e290dd00dd3f9a94a607d31bb5da73",
+    "url": "/static/media/KnowYourRestautant.21e290dd.webp"
+  },
+  {
+    "revision": "429cbce710323361e6b0a2aae104afe7",
+    "url": "/static/media/Profile2.429cbce7.jpg"
+  },
+  {
+    "revision": "bf989778565c49da91f9e0ef13dbdc5c",
+    "url": "/static/media/background.bf989778.webp"
+  },
+  {
+    "revision": "e95d05630894e5403e569fdaa0a2b61a",
+    "url": "/static/media/background.e95d0563.webp"
+  },
+  {
+    "revision": "ffded2381d25dd906406d6e07f91dea5",
+    "url": "/static/media/bootstrap-4.ffded238.svg"
+  },
+  {
+    "revision": "5a66aae04e8d05c087106b39bdf9f8df",
+    "url": "/static/media/covid-19tracker.5a66aae0.webp"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "/static/media/css3.3707da3c.svg"
+  },
+  {
+    "revision": "1190a19ce3cc940741a60137a1e074cb",
+    "url": "/static/media/git-icon.1190a19c.svg"
+  },
+  {
+    "revision": "0ff1a6f08182be4aa63a0b54da1b9e57",
+    "url": "/static/media/github.0ff1a6f0.svg"
+  },
+  {
+    "revision": "b38ff61aa327fbdc24f7b1dad7d04d1a",
+    "url": "/static/media/heroku.b38ff61a.svg"
+  },
+  {
+    "revision": "676e8188f003ed5d4882a0d0f378dad3",
+    "url": "/static/media/html-5.676e8188.svg"
+  },
+  {
+    "revision": "4e1c177ea9d48abb97050fec0e0b3fdd",
+    "url": "/static/media/images2.4e1c177e.png"
+  },
+  {
+    "revision": "13a096531e50277a3fb17460c39db221",
+    "url": "/static/media/java.13a09653.svg"
+  },
+  {
+    "revision": "2d2e31cc63a96c7e6044f52d30fd21b9",
+    "url": "/static/media/javascript.2d2e31cc.svg"
+  },
+  {
+    "revision": "82291019f34ab54a677c456a84127cc3",
+    "url": "/static/media/material-ui-1.82291019.svg"
+  },
+  {
+    "revision": "54d7bddcd3019ec3c3b23db00160ccef",
+    "url": "/static/media/mssql.54d7bddc.svg"
+  },
+  {
+    "revision": "66e834b24ca3d4b0175a6a3eec86ae90",
+    "url": "/static/media/netlify.66e834b2.svg"
+  },
+  {
+    "revision": "ef194ddf5faf63cb6cc580a4535be695",
+    "url": "/static/media/react-bootstrap.ef194ddf.svg"
+  },
+  {
+    "revision": "024196716802e272adb3cbefb81fd557",
+    "url": "/static/media/react-router.02419671.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "aa0a337603611296524a666b62a9ff23",
+    "url": "/static/media/redux.aa0a3376.svg"
+  },
+  {
+    "revision": "8bce7aef886c3872c218e0e6ef8bdccb",
+    "url": "/static/media/slide1.8bce7aef.webp"
+  },
+  {
+    "revision": "7256f309520276be70acd97720610bc6",
+    "url": "/static/media/slide2.7256f309.webp"
+  },
+  {
+    "revision": "8e5b87b60151049a8df9ec0bd30978f0",
+    "url": "/static/media/slide3.8e5b87b6.webp"
+  }
+]);
